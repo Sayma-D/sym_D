6111ad103eb7094760b20e852a4f22fc332b6b12
sym <sayma16510@gmail.com>
2024-01-23 08:54:27 +0530

    conversions of datatypes

---
+++ 1_basics/4_conversionsOperations.js
@@ -0,0 +1,36 @@
+let score = "sayma"
+
+// console.log(typeof score); // Here we used the score as 35 first the result says it is number then we used double quotes then it says it is string.
+// console.log(typeof(score));
+
+// Then we wanted the value in number so we tried the number like example given below n it said the number is number. 
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// Then we wrote the only value in number n it said it is NoN (not a number) sometimes the node or js makes mistakes like this but there e some other ways to get the answers too.
+// console.log(valueInNumber);
+
+// Then we tried the null the node said it is 0 then we tried boolean it said it is 1 cuz it is true n if we had written the false then the answer would be 0. 
+
+// Then we wrote the value that's not the number so the answer was again NoN (not a number).
+
+let isLoggedIn = "sayma"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// As per the example upper below when we wrote the true by using boolean we got the answer as 1 n when we used the value as 0 we got the answer false. 
+
+// When we wrote the value as ("") empty double quotes then we got the value as false but when we wrote the value as ("sayma") when we wrote something in that place we got the answer as true.
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+// Here we wrote some number as 30 and we used string the answer we got was 30.
+
+console.log(typeof stringNumber);
+// When we used the typeof to check if it got converted to string or not we got the answer as the string. 
+
+ 
