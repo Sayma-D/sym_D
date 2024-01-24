7c7e0c3b09a55a562c9914074b5709ab0395264e
sym <sayma16510@gmail.com>
2024-01-24 08:55:13 +0530

    comparisions

---
+++ 1_basics/3_datatypes.js
@@ -0,0 +1,49 @@
+"use strict" // it is used as the newer version of java script.
+
+// alert(3 + 3) node doesn't support the arthmetic operations like addition or minus or so on. the people who use this will write it in html file by binding it they will write it in console by inspecting it. the engine of the java script is hidden in browser.
+
+// the code needs to be readable for e.x - 
+
+// console.log(3 + 3) console.log("sayma") // the error will be shown between them but if u put the semicolon between them them it won't but thats not the solution u have to write the next line to new line so that code can be readable.
+
+console.log(3 
+    + 3)
+
+console.log("sayma")
+
+// the results will be shown but it will be better not to use it cuz code needs to readable and clear.
+
+let name = "sayma"
+// it is knows as string.
+
+let age = 19
+// it is nown as the numbers value. 
+
+let isLoggedIn = true
+// it is known as the Boolean. 
+
+
+// we will know how many of the premitive datatypes we will have:
+
+// 1. number = 2 to the power of 53 means the minimum safe integer is this. 
+
+// 2. bigInt = the bigint is used for the bigger numbers specially in trading or in stock markets or in big websites like raddit or in facebookor so on,  but in js code we won't be using the bigint much.
+
+// 3. string = it is used in single quotes or in double quotes but it will be better to use it in double quotes. 
+
+// 4. boolean = it is used for true or false , if the ans is true then true or false. 
+
+// 5. null = it is standalone value. 
+
+// 6. undefined = it means that  have not written the value the deffrence between null and undefined is that if u r writing the variable n not putting its value then it means node will declare it as undefined but null means if u r putting a null instead of the value it means u r asking the node or server for the empty space for value. 
+
+// 7. symbol = it is used to find the uniqueness of the value. it is also used to find the individual component of the variable.
+
+// 8. object = //
+
+
+// console.log(typeof "sayma"); it will be shown as the string by the node Js. 
+// console.log(typeof age); it will be shown as the number by the node. 
+// console.log(typeof null); it will be shown as the object cuz it is the object in Node.
+// console.log(typeof undefined); it will be shown as the undefined cuz it is the type in Node. 
+
--- 1_basics/4_conversionsOperations.js
+++ 1_basics/4_conversionsOperations.js
@@ -17,8 +17,8 @@ let score = "sayma"
 
 let isLoggedIn = "sayma"
 
-let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // As per the example upper below when we wrote the true by using boolean we got the answer as 1 n when we used the value as 0 we got the answer false. 
 
@@ -26,11 +26,70 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 30
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
 // Here we wrote some number as 30 and we used string the answer we got was 30.
 
 console.log(typeof stringNumber);
 // When we used the typeof to check if it got converted to string or not we got the answer as the string. 
 
+//  ************************************************* : OPERATIONS : **********************************************************
+
+let value = 4
+// let negValue = -value
+// console.log(negValue);
+
+// when we wrote the negvalue to find the negvalue we wrote -value of negvalue the answer was -4.
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+
+// As per the example given as upper we can get the answers like that too. 
+
+let str1 = "welcome!"
+let str2 = " sayma";
+
+let str3 = str1 + str2
+console.log(str3);
+
+// Here we gave the str1 and str2 a name and we added them and gave them a name as str3 and using console.log we goe the answer.
+
+
+console.log("1" + 2); // Here we gave the quotation mark to 1 n we got the answer as 12.
+console.log(1 + "2"); // Here we gave the quotation mark to 2 n we got the answer as 12. 
+console.log("1" + 2 + 2); // Here we gave quotation mark to 1 n added another 2 n we got the answer as 122.
+console.log(1 + 2 + "2"); // Here we gave the quotation mark to 2 and got the answer as 32.
+console.log(1 + "2" + 2); // Here we gave the quotation mark to 2 and got the answer as 122.
+
+// Do not be dependent on node for this type of methods of arthmetic operations.  
+
+// u will see many examples of js in exams or on internet like :
+
+// console.log(2 + 3 * 4 % 2);
  
+// DON'T use this type of codes in real life its ok for the exams or for the questions but not in real life coding avoid writing it like that instead u can write like this :
+
+// console.log((2 + 3) * 4); // Use the perenthesis () for using this method.
+//  we can get the answers by first adding it n then multiplying its answer to another value like that u can use this method. 
+
+console.log(true); // Here we wrote the true and we got the answer true as expected.
+console.log(+ true); // Here we wrote the + true and we got the answer as 1.
+console.log(+ ""); // Here we wrote this value and we got the answer as 0. this is not the perfect way of writing code.
+
+// let num1 , num2 , num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// Here we wrote the 3 numbers equally and showed it as a single number has a value as 4 , we dont need to write this type of code cuz the code needs to be readable thats not the right Way.
+
+let gameCounter = 101 // As per we wrote the value as 101 and we used the addition n we got the answer as 102. 
+// ++gameCounter; // This is the prefix increment.
+gameCounter++; // we used the addition before and after the variable n got the same answer. this is postfix increment.
+console.log(gameCounter);
+
+// The increment (++) operator increments (adds one to) its operand and returns the value before or after the increment, depending on where the operator is placed.
